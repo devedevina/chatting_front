ccdf340ddcedd68e31208716fd626253f913a659
ina <cnsang166@gmail.com>
2026-05-27 23:15:43 +0900

    fix: 채팅방 생성시 비공개 체크되는 부분 수정

--- src/components/ChatRoom/ChatRoomCreate.jsx
+++ src/components/ChatRoom/ChatRoomCreate.jsx
@@ -8,7 +8,7 @@ export default function ChatRoomCreate({ onClose, onCreated }) {
   const [formData, setFormData] = useState({
     title: '',
     description: '',
-    isPublic: true,
+    isPublic: false,
     requiresPassword: false,
     password: '',
     maxMembers: 50,
